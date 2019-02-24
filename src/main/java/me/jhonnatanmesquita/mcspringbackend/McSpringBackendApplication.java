fdfe4447b8c0fb7574d3a142a164b96dad3d339a
jhonnatanmesquita <jhonnatanmesquita@gmail.com>
2019-02-23 23:54:16 -0300

    Initial commit

---
+++ src/main/java/me/jhonnatanmesquita/mcspringbackend/McSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package me.jhonnatanmesquita.mcspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McSpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jhonnatanmesquita/mcspringbackend/McSpringBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package me.jhonnatanmesquita.mcspringbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class McSpringBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
