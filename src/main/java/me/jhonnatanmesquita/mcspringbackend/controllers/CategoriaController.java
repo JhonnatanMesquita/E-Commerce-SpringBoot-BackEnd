c3d1def1098dd63c49aed3b2253b1e8c186cb7e7
Jhonnatan Mesquita <jhonnatanmesquita@gmail.com>
2019-07-26 15:39:33 -0300

    Iniciando Operações de CRUD
    #Implementando a inserção de Categoria pelo POST

--- src/main/java/me/jhonnatanmesquita/mcspringbackend/controllers/CategoriaController.java
+++ src/main/java/me/jhonnatanmesquita/mcspringbackend/controllers/CategoriaController.java
@@ -4,10 +4,10 @@ import me.jhonnatanmesquita.mcspringbackend.models.Categoria;
 import me.jhonnatanmesquita.mcspringbackend.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -22,6 +22,13 @@ public class CategoriaController {
         Categoria obj = service.buscar(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> Insert(@RequestBody Categoria obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
