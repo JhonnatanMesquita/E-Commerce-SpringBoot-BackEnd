86f24458e06cdf5245a723b1e9a4acb335e4eab7
Jhonnatan Mesquita <jhonnatanmesquita@gmail.com>
2019-08-19 13:12:29 -0300

    Implementação da validação de email, verifica se o email já existe no sistema

--- src/main/java/me/jhonnatanmesquita/mcspringbackend/dao/ClienteDao.java
+++ src/main/java/me/jhonnatanmesquita/mcspringbackend/dao/ClienteDao.java
@@ -3,10 +3,11 @@ package me.jhonnatanmesquita.mcspringbackend.dao;
 import me.jhonnatanmesquita.mcspringbackend.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteDao extends JpaRepository <Cliente, Integer>{
 
-
-
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
--- src/main/java/me/jhonnatanmesquita/mcspringbackend/services/validation/ClienteInsertValidator.java
+++ src/main/java/me/jhonnatanmesquita/mcspringbackend/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package me.jhonnatanmesquita.mcspringbackend.services.validation;
 
+import me.jhonnatanmesquita.mcspringbackend.dao.ClienteDao;
 import me.jhonnatanmesquita.mcspringbackend.dto.ClienteNewDTO;
 import me.jhonnatanmesquita.mcspringbackend.enums.TipoCliente;
 import me.jhonnatanmesquita.mcspringbackend.exceptions.FieldMessage;
+import me.jhonnatanmesquita.mcspringbackend.models.Cliente;
 import me.jhonnatanmesquita.mcspringbackend.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.FieldError;
 
 import javax.validation.ConstraintValidator;
@@ -12,6 +15,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteDao repo;
 
     @Override
     public void initialize(ClienteInsert ann){
@@ -27,6 +33,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
+        }
+
+        Cliente aux = repo.findByEmail((objDto.getEmail()));
+
+        if(aux != null) {
+            list.add(new FieldMessage("email", "Este e-mail já existe"));
         }
 
         for (FieldMessage e : list){
