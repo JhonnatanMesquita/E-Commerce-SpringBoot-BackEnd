f40a18527e46a9a3b5225779b3e789d604a37f1c
Jhonnatan Mesquita <jhonnatanmesquita@gmail.com>
2019-08-19 17:02:43 -0300

    Implementação da validação de email e ID ao realizar o update de um cliente

--- src/main/java/me/jhonnatanmesquita/mcspringbackend/dto/ClienteDTO.java
+++ src/main/java/me/jhonnatanmesquita/mcspringbackend/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package me.jhonnatanmesquita.mcspringbackend.dto;
 
 import me.jhonnatanmesquita.mcspringbackend.models.Cliente;
+import me.jhonnatanmesquita.mcspringbackend.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
     private static final long serialVersionUID = 1l;
---
+++ src/main/java/me/jhonnatanmesquita/mcspringbackend/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,54 @@
+package me.jhonnatanmesquita.mcspringbackend.services.validation;
+
+import me.jhonnatanmesquita.mcspringbackend.dao.ClienteDao;
+import me.jhonnatanmesquita.mcspringbackend.dto.ClienteDTO;
+import me.jhonnatanmesquita.mcspringbackend.dto.ClienteNewDTO;
+import me.jhonnatanmesquita.mcspringbackend.enums.TipoCliente;
+import me.jhonnatanmesquita.mcspringbackend.exceptions.FieldMessage;
+import me.jhonnatanmesquita.mcspringbackend.models.Cliente;
+import me.jhonnatanmesquita.mcspringbackend.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private ClienteDao repo;
+
+    @Override
+    public void initialize(ClienteUpdate ann){
+
+    }
+
+    public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context){
+
+        @SuppressWarnings("unchecked")
+        Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Integer uriId = Integer.parseInt(map.get("id"));
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        Cliente aux = repo.findByEmail((objDto.getEmail()));
+
+        if(aux != null && !aux.getId().equals(uriId)) {
+            list.add(new FieldMessage("email", "Este e-mail já existe"));
+        }
+
+        for (FieldMessage e : list){
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+
+        return list.isEmpty();
+    }
+}
