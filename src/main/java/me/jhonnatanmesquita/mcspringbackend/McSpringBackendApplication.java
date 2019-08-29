592c62c1dde08526a925b3641049a78a3582f76e
Jhonnatan Mesquita <jhonnatanmesquita@gmail.com>
2019-08-29 13:33:36 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/me/jhonnatanmesquita/mcspringbackend/McSpringBackendApplication.java
+++ src/main/java/me/jhonnatanmesquita/mcspringbackend/McSpringBackendApplication.java
@@ -1,5 +1,7 @@
 package me.jhonnatanmesquita.mcspringbackend;
 
+import me.jhonnatanmesquita.mcspringbackend.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class McSpringBackendApplication implements CommandLineRunner {
 
-
+    @Autowired
+    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(McSpringBackendApplication.class, args);
@@ -15,6 +18,6 @@ public class McSpringBackendApplication implements CommandLineRunner {
 
     @Override
     public void run (String... args) throws Exception{
-
+        s3Service.uploadFile("C:\\temp\\imgage.jpg");
     }
 }
