63e971fcfba638fb234f6f4bcb09bf7387ee7b53
Jhonnatan Mesquita <jhonnatanmesquita@gmail.com>
2019-09-01 13:59:00 -0300

    Padronizando o formato das exceções personalizadas com as exceções do Framework

--- src/main/java/me/jhonnatanmesquita/mcspringbackend/services/CidadeService.java
+++ src/main/java/me/jhonnatanmesquita/mcspringbackend/services/CidadeService.java
@@ -3,9 +3,11 @@ package me.jhonnatanmesquita.mcspringbackend.services;
 import me.jhonnatanmesquita.mcspringbackend.models.Cidade;
 import me.jhonnatanmesquita.mcspringbackend.repositories.CidadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CidadeService {
 
     @Autowired
